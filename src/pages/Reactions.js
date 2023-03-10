17dd755f4fe0598d0fd145c35413fa90de9bc794
Hannah Jin <hannahjin@s-169-232-116-39.resnet.ucla.edu>
2023-03-09 18:21:31 -0800

    hola

--- src/pages/Reactions.js
+++ src/pages/Reactions.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import alkyne from '../images/alkyne1.png';
 import alkene1 from '../images/alkene1.png';
+import alkene2 from '../images/alkene2.png';
 
 function Reactions() {
     const [GoToReactions, setGoToReactions] = useState(false);
@@ -15,8 +16,12 @@ function Reactions() {
                     <div>
                         <p><b> Trans Alkene <span>&#40;</span>Birch Reduction<span>&#41;</span></b></p>
                         <img src={alkene1} />
-                        <p>Birch Reduction, or also called dissolving metal reduction, used to turn alkyne into trans alkene.</p>
-
+                        <p>Birch Reduction, or also called dissolving metal reduction, is used to turn alkyne into a trans alkene.</p>
+                        <p><b>Cis Alkene</b></p>
+                        <img src={alkene2} />
+                        <p><i>Requires alkyne</i></p>
+                        <p>H<sub>2</sub> is added on the same side across the pi bond. The alkyne turns into a cis alkene and the addition of Lindlar<span>&#39;</span>s catalyst prevents further reduction into an alkane.</p>
+                        <p><b></b></p>
                     </div>
                 )
             // case 'alkynes':
