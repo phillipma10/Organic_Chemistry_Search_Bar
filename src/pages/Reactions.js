ea8fb6dad9d928e36dc309f995ce91df1d1bec0c
Your Name <you@example.com>
2023-03-09 19:07:31 -0800

    newest

--- src/pages/Reactions.js
+++ src/pages/Reactions.js
@@ -26,23 +26,32 @@ function Reactions() {
                         <p><b> Trans Alkene <span>&#40;</span>Birch Reduction<span>&#41;</span></b></p>
                         <img src={alkene1} />
                         <p>Birch Reduction, or also called dissolving metal reduction, is used to turn alkyne into a trans alkene.</p>
-                        <p><b>Cis Alkene</b></p>
+                        <p><b>Cis Alkene </b></p>
                         <p><i>Requires alkyne</i></p>
                         <img src={alkene2} />
                         <p>H<sub>2</sub> is added on the same side across the pi bond. The alkyne turns into a cis alkene and the addition of Lindlar<span>&#39;</span>s catalyst prevents further reduction into an alkane.</p>
                         <p><b>Aldol Condensation</b></p>
                         <img src={alkene3} />
                         <p>Adding heat helps to pop off an -OH group.</p>
-                        
+                        <p><b>Generally Making Alkenes</b></p>
+                        <p><i>E2 Reaction</i></p>
+                        <img src={alkene4} />
+                        <p>Potassium tert-butoxide is a strong base, which pushes towards an E2 mechanism, enabling the creation of an alkene.</p>
                     </div>
                 )
-            // case 'alkynes':
-            //     return(
-            //         <div>
-            //             <img src={} />
-            //             <p></p>
-            //         </div>
-            //     )
+            case 'alkanes':
+                return(
+                    <div>
+                        <p><b>Full Reduction from Alkyne</b></p>
+                        <p><i>Requires alkyne</i></p>
+                        <img src={alkane1} />
+                        <p>H<sub>2</sub> is added on the same side across the pi bond. The alkyne turns into a cis alkene that reacts again with H<sub>2</sub>, causing a further reduction. However, the reduction can be stopped at the alkene if Lindlar<span>&#39;</span>s catalyst is added.</p>
+                        <p><b>Catalytic Hydrogenation</b></p>
+                        <p><i>Syn addition of hydrogens</i></p>
+                        <img src={alkane2} />
+                        <p>Here you are reducing an alkene into an alkane by a syn addition of hydrogens. In order for this reaction to proceed, a catalyst such as Pd/C, Pt/C, or Ni/C is required. The catalyst consists of transition metals, which hold onto their electrons loosely, enabling this reaction to occur.</p>
+                    </div>
+                )
             default:
             return null
         }
